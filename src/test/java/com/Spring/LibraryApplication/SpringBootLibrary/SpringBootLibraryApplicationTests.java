3ee57ebcd2ce29249fa8c7aab47ad93e7cc4da76
Crystal Cox <crystal.cox@cgius.com>
2017-08-15 14:31:26 -0400

    init

---
+++ src/test/java/com/Spring/LibraryApplication/SpringBootLibrary/SpringBootLibraryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.Spring.LibraryApplication.SpringBootLibrary;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootLibraryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
